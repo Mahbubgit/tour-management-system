e751520fb3798d729e448fa58b75ce45ec4a9709
Your Name <you@example.com>
2022-09-20 22:15:33 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Tour Management System.')
+})
+
+app.listen(port, () => {
+    console.log(`Tour Management System app listening on port ${port}`)
+})
